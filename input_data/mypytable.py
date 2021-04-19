e9540a6de4e56a3b486a0d7daf761d0ee275ae73
Joseph Torii <jtorii@zagmail.gonzaga.edu>
2021-04-19 23:23:52 +0000

    setting up files

---
+++ input_data/mypytable.py
@@ -0,0 +1,27 @@
+####################################################
+# Names: Josephy Torii and Alex Giacobbi
+# Professor: Sprint
+# Class: CPSC 322
+# Date: 19 April 2021
+#
+#
+# Description: 
+####################################################
+
+
+
+class MyRandomForestClassifier:
+    '''
+
+    '''
+
+
+
+    def __init__(self):
+        '''
+        Args:
+        Returns:
+        '''
+        pass
+
+    
