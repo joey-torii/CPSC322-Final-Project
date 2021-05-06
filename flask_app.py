4e7936ba425d49126f850ad2aa45ac5c39dc5b30
Alex Giacobbi <agiacobbi@zagmail.gonzaga.edu>
2021-05-06 00:28:15 +0000

    sdlkjfsdkljf

--- flask_app.py
+++ flask_app.py
@@ -124,4 +124,6 @@ def get_spectral_class(data):
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+    # app.run()
